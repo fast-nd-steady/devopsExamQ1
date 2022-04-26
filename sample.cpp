5fe0cb31590e552432597c34c266e7359417fc30
Yash <yash.ag.bu@gmail.com>
2022-04-26 09:48:19 +0530

    sample cpp file pushed

---
+++ sample.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define m1 1000000007
+#define m2 1000000009
+#define f(i, a, b) for (int i = a; i < b; i++)
+#define sortv(a) sort(a.begin(), a.end())
+#define sorta(a, n) sort(a, a + n)
+#define pb push_back
+#define maxa(a, n) *max_element(a, a + n)
+#define mina(a, n) *min_element(a, a + n)
+#define revv(v) reverse(v.begin(), v.end())
+#define sumv(v) accumulate(v.begin(), v.end(), 0)
+#define suma(a, n) accumulate(a, a + n, 0);
+#define maxv(v) *max_element(v.begin(), v.end())
+#define minv(v) *min_element(v.begin(), v.end())
+#define ll long long
+#define int long long
+#define vi vector<int>
+#define vii vector<vector<int>>
+#define pf push _front
+#define io                            \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+int32_t main()
+{
+    io;
+    int TC;
+    TC = 1;
+    cin >> TC;
+    while (TC--)
+    {
+    }
+}
